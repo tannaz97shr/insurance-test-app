dd4996bf1c85a9353f061b95031abbcb9295c8ab
Tannaz Shirzadi <tia.shr97@gmail.com>
2025-03-17 20:02:25 +0300

    feat: fetch table data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { fetchApplications } from "../api";
+import { IFormData } from "../types/general";
 
 const Home = () => {
   const navigate = useNavigate();
+
+  const [applications, setApplications] = useState<IFormData[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getApplications = async () => {
+      try {
+        const data = await fetchApplications();
+        setApplications(data);
+      } catch (error) {
+        console.error("Error fetching applications:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getApplications();
+  }, []);
+
+  console.log(applications);
+
+  if (loading) return <p>Loading applications...</p>;
 
   return (
     <div>
